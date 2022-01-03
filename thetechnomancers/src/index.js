a604fe6ded07784fd1a9a310c923741dfa244215
Jonesey712 <monique.jones712@gmail.com>
2022-01-02 22:27:47 -0600

    built splash page and adding blog

--- thetechnomancers/src/index.js
+++ thetechnomancers/src/index.js
@@ -1,17 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from "react-router-dom";
+import AllPosts from "./components/AllPosts.js";
+import OnePost from "./components/OnePost.js";
 import './index.css';
+import cj from './images/cj_photo.jpg';
+import mo from './images/mo_photo.jpg';
 
-class HomePage extends React.Component {
-    render() {
-        return (
-            <div className='home'>
-                <h1>The Technomancers</h1>
-            </div>
-        );
-    }
+function App (props) {
+    return ( 
+        <BrowserRouter>
+        <div>
+            <Route component={AllPosts} path='/' exact />
+            <Route component={OnePost} path='/:slug' />
+        </div>
+        </BrowserRouter>
+
+        <div className='home'>
+            <h1 href='/'>The Technomancers</h1>
+            <h2>Welcome to our site! Plese click on a picture to see the resume.</h2>
+            <a href='/jones-c'>
+                <img className='picture-cj'
+                    src={cj} alt='cj'
+                /> Charles Jones
+            </a>
+            <a href='/jones-m'>
+                <img className='picture-mo'
+                    src={mo} alt='mo'
+                /> Monique Jones
+            </a>
+        </div>
+    );
 }
 
 ReactDOM.render(
+    <App />,
     document.getElementById('root')
 );
+
+export default App;
